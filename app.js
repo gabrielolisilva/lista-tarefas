7770c9a8109b57c26859ca6025591bdb40f1a850
gabrielolisilva <gabrielsilva2799@hotmail.com>
2022-11-24 10:27:25 -0300

    Controllers and Routes addition

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log("Task Manager App");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const tasks = require("./routes/tasks");
+
+//middleware
+
+app.use(express.json());
+
+// routes
+app.get("/", (req, res) => {
+  res.status = 200;
+  res.send("Hello world");
+});
+
+app.use("/api/v1/tasks", tasks);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}...`);
+});
