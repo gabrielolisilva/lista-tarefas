d15e51159cf001dbe2f1ba07235a999f5ca5fe84
gabrielolisilva <gabrielsilva2799@hotmail.com>
2022-11-25 14:11:05 -0300

    404 page addition

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 3000;
 const connectDB = require("./db/connect");
 require("dotenv").config();
+const notFound = require("./middleware/not-found");
 
 const tasks = require("./routes/tasks");
 
@@ -11,8 +12,8 @@ app.use(express.static("./public"));
 app.use(express.json());
 
 // routes
-
 app.use("/api/v1/tasks", tasks);
+app.use(notFound);
 
 const startServer = async () => {
   try {
