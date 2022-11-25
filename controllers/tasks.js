2b9957301aa4559f4dede0e9349f86182af5b370
gabrielolisilva <gabrielsilva2799@hotmail.com>
2022-11-25 10:22:58 -0300

    Get single task and delete task addition

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -14,24 +14,40 @@ const getAllTasks = async (req, res) => {
 const createTask = async (req, res) => {
   try {
     const task = await Task.create(req.body);
-    res.status(201);
+    res.status = 201;
     res.json({ task });
   } catch (err) {
-    res.status(500);
+    res.status = 500;
     res.json({ msg: err });
   }
 };
 
-const getTask = (req, res) => {
-  res.json({ id: req.params.id });
+const getTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+    if (!task) res.status(404).json({ msg: `No task with this id` });
+    res.json({ task });
+  } catch (err) {
+    res.status = 500;
+    res.json({ msg: err });
+  }
 };
 
 const updateTask = (req, res) => {
   res.send("update task");
 };
 
-const deleteTask = (req, res) => {
-  res.send("delete task");
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+    if (!task) res.status(404).json({ msg: `No task with this id` });
+    res.json({ task });
+  } catch (error) {
+    res.status = 500;
+    res.json({ msg: err });
+  }
 };
 
 module.exports = {
